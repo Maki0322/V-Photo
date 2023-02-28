389722dceac721c4116b3d0fa785680660fb87a9
maki <murasame.asa.73@gmail.com>
2023-03-01 00:39:45 +0900

    #16 add: ログアウトした際にprofileのrecoilの値を初期化する

--- components/LogoutModal.tsx
+++ components/LogoutModal.tsx
@@ -12,10 +12,14 @@ import { userAuthState } from '../state/userAuthState';
 import { logoutCompleteModalShowState } from '../state/logoutCompleteModalShowState';
 import { MyMuiRoundButton } from './atoms/buttons/MyMuiRoundButton';
 import { MyMuiSquareButton } from './atoms/buttons/MyMuiSquareButton';
+import { profileState } from '../state/profileState';
 
 const LogoutModal = () => {
   const router = useRouter();
   const setUserAuth = useSetRecoilState(userAuthState);
+
+  // プロフィール情報をuseRecoilで管理
+  const [profile, setProfile] = useRecoilState(profileState);
 
   // ログアウトモーダルの値をrecoilで管理
   const [logoutModalShow, setLogoutModalShow] = useRecoilState(logoutModalShowState);
@@ -31,8 +35,16 @@ const LogoutModal = () => {
     await signOut(auth);
     await setUserAuth(false);
     await router.push("/");
+    setProfile({
+      userName: "No Name",
+      userMemo: "よろしくお願いします。",
+      userPickUpPhoto: "https://firebasestorage.googleapis.com/v0/b/v-photo.appspot.com/o/image%2Fno_image.png?alt=media&token=bf785fef-6b0a-4640-9a22-4be838549468",
+      userPickUpDescription: "",
+      userIcon: "https://firebasestorage.googleapis.com/v0/b/v-photo.appspot.com/o/image%2Ficon3.png?alt=media&token=433d387c-2bcb-4f25-9593-a05b5d4dcb84",
+    })
     setLogoutCompleteModalShow(true);
   };
+
 
 
   if(logoutModalShow) {
@@ -43,12 +55,12 @@ const LogoutModal = () => {
             <MyCloseIcon onClick={closeLogoutModal}/>
             <div>ログアウトしますか？</div>
             <Link href={"/"} style={{textDecoration: "none"}}>
-              <MyMuiSquareButton
+              <MyMuiRoundButton
                 onClick={handleLogout}
                 sx={{width:"150px"}}
               >
                 ログアウト
-              </MyMuiSquareButton>
+              </MyMuiRoundButton>
             </Link>
           </div>
         </div>
